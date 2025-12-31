0261d04cbfae3d603ddf7aedfacdb1aef70670a3
CalvinWill <98561276+CalvinWill@users.noreply.github.com>
2025-12-31 15:19:32 -0500

    AllDocs

--- src/app/Components/GameWrapper.tsx
+++ src/app/Components/GameWrapper.tsx
@@ -31,11 +31,11 @@ const gameItems: GameItem[] = [
         position: { top: "20%", left: "25%" }
     },
     {
-        id: 3,
+        id: 2,
         imageSrc: ChartersVA.src,
         title: "Charters of the Virginia Company of London",
         year: "1606",
-        description: "This is the description for item 2.",
+        description: "This document guaranteed the rights of Englishmen, and it focuses on the newfound colonies in North America. The king had realized that England was running short on resources, and with a desire for wealth, decided to send some of his citizens across the ocean in search of God, Gold, and Glory. Since men were about to settle in “The New World,” they needed money, and a lot of it. This document allowed Englishmen to invest in Jamestown, fund the trip, and give the colonists money for land and basic needs. It also ensured that all people in the new colonies had the same rights they did in England. Allowing the citizens of Jamestown to self-govern, and elect officials, this document is a great example of Popular Sovereignty.",
         position: { top: "20%", left: "40%" }
     },
     {
@@ -43,7 +43,7 @@ const gameItems: GameItem[] = [
         imageSrc: VADeclaration.src,
         title: "Virginia Declaration of Rights",
         year: "1776",
-        description: "This is the description for item 3.",
+        description: "Being a basis for three of America’s most important foundational documents makes The VA Declaration of Rights extra special in my opinion. Written by George Mason, it was made during the Revolutionary War for the purpose of creating a new government that would follow the principles the founding fathers believed in. It gave Virginians rights considered “unalienable,” such as having the freedom to follow any faith you want, and trial by jury. This document introduced natural rights such as, “Life and Liberty,” to North America, and served as a prime example of American constitutionalism, meaning that the government's power should be limited in order to protect the rights of citizens. All these things point to the fundamental principle, Individual Rights, since it guarantees freedoms America believes that all people should have.",
         position: { top: "20%", left: "54%" }
     },
     {
@@ -51,7 +51,7 @@ const gameItems: GameItem[] = [
         imageSrc: articlesConfederation.src,
         title: "Articles of Confederation",
         year: "1776",
-        description: "This is the description for item 4.",
+        description: "This document, while flawed, created our first national government. It was weak at the federal level, and gave states too much power. It was adopted during the Revolutionary war, similar to both the VA Declaration of Rights and the Declaration of Independence. The country was recently freed from the king, and didn’t want there to be too much power in the hands of central authority, so they made a government where the states could be more independent, but it actually did more harm than good. This document represents Federalism, because the powers were unevenly spread out between the states and central government. Overall, it mostly served as a guide for what NOT to do, helping the future government try and keep a proper balance between the state and national level.",
         position: { top: "20%", left: "68%" }
     },
     {
@@ -59,7 +59,7 @@ const gameItems: GameItem[] = [
         imageSrc: Declaration.src,
         title: "Declaration of Independence",
         year: "1776",
-        description: "This is the description for item 5.",
+        description: "Arguably America’s most famous document, it announced our separation from Britain and our country’s Independence. King George the third had been giving the colonies little representation in government and unfair taxes. (much like his distant cousin, King John.) The colonists, being dissatisfied, wrote this document stating their grievances for the king, and what they were going to do about it.This document shifted the people’s focus from serving a king, to protecting the rights they considered unalienable, and it united them all together as a country. Popular Sovereignty represents this document best, as the people are the source of power for the government our founding fathers built.",
         position: { top: "50%", left: "25%" }
     },
     {
@@ -67,7 +67,7 @@ const gameItems: GameItem[] = [
         imageSrc: ReligiousFreedom.src,
         title: "Virginia Statute of Religious Freedom",
         year: "1786",
-        description: "This is the description for item 6.",
+        description: "Mostly written by Thomas Jefferson, it was created to abolish the idea of a state having an official religion. The Church of England was the official church in the colonies since the Charters of The VA Company of London brought it over along with the same rights for colonists as the rights of citizens of England. It required taxes, and attendance on Sunday’s, therefore forcing the people in the colonies into a religion that might not be their own. This document inspired the First Amendment of the Bill of Rights, and it was also the first time religious freedom was protected by law in the history of the modern world. It represents the principle of Individual Rights as it protects a right that’s considered unalienable.",
         position: { top: "50%", left: "40%" }
     },
     {
@@ -75,7 +75,7 @@ const gameItems: GameItem[] = [
         imageSrc: Constitution.src,
         title: "United States Constitution",
         year: "1787",
-        description: "This is the description for item 7.",
+        description: "The Constitution is the basis for our current government. It was written while America was under the Articles of Confederation which had a weak central government and threatened the survival of the new country. Knowing this, leaders came and gathered at the Constitutional Convention in hope of revising the Articles of Confederation, but decided to draft a new document instead. The Constitution created shared power between the state and national governments, made and divided our three branches of government, and allowed them to restrict each other’s power. This document would represent both Separation of Powers and Checks and Balances because of these reasons.",
         position: { top: "50%", left: "54%" }
     },
     {
@@ -83,7 +83,7 @@ const gameItems: GameItem[] = [
         imageSrc: BillOfRights.src,
         title: "Bill of Rights",
         year: "1789",
-        description: "This is the description for item 8.",
+        description: "The preamble to the Constitution, The Bill of Rights helped establish the structure of our current government. It protects our fundamental freedoms such as freedom of religion, press, and speech. The Bill of Rights were written just as America was debating the Constitution, fearing that it would make too strong of a central government, as opposed to the weak central government established under the Articles of Confederation. The citizens fearing this were called Anti-Federalists. This eruption of debate was what prompted James Madison to write the first draft of the Bill of Rights. This is an example of Federalism, as they were trying to get a balance between the state and federal governments.",
         position: { top: "50%", left: "68%" }
     }
 ];
